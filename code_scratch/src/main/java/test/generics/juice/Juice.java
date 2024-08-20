5a924273031463a329ca8c340ea4be4da070f2e1
sue <shoh@maxst.com>
2024-08-20 11:34:14 +0900

    Reflection test

--- code_scratch/src/main/java/test/generics/juice/Juice.java
+++ code_scratch/src/main/java/test/generics/juice/Juice.java
@@ -2,6 +2,22 @@ package test.generics.juice;
 
 public class Juice {
     String name;
+
+    private int quantity;
+
+    protected static boolean isEnabled;
+
+    private Juice(){}
+
     public Juice(String name) {this.name = name + "Juice";}
+
     public String toString() {return name;}
+
+    public void getJuice() {
+        StringBuilder sb = new StringBuilder("name : ");
+        sb.append(this.name)
+            .append("\nquantity : ").append(quantity)
+            .append("\nis enabled : ").append(isEnabled);
+        System.out.printf(sb.toString());
+    }
 }
